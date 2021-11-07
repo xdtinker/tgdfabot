f3c8112087d7f7d7f0afc1f59579af166a4af96b
xdtinker <93765440+xdtinker@users.noreply.github.com>
2021-11-07 08:57:24 +0600

    Iinitial Commit stable v1

--- bot.py
+++ bot.py
@@ -1,113 +1,9 @@
-import os
 import telebot
 import constant as keys
-import smtplib
-import time
-import os
-import requests
-from selenium.webdriver.support.color import Color
-from selenium import webdriver
-from datetime import datetime
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import TimeoutException
+import dfa as main
+
 
 bot = telebot.TeleBot(keys.API_KEY)
-chrome_options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-
-site = "https://www.passport.gov.ph/appointment"
-path = "C:/Users/Aziz/Desktop/Automation/chromedriver.exe"
-
-def sendTelegram(botMsg):
-    bot_token = "bot2023896048:AAE_MnkOljwcRXNXlC6ouEwrTpfYZVeRc1c"
-    bot_ChatID = "879252455"
-    bot_text = f'https://api.telegram.org/{bot_token}/sendMessage?chat_id={bot_ChatID}&text={botMsg}'
-
-    response = requests.get(bot_text)
-    
-def tgGetLogs(botLogs):
-    bot_token = "bot2054859695:AAGVSXp1MRtrAMP0L5g2AML-tBVvwRfxi4o"
-    bot_ChatID = "879252455"
-    bot_text = f'https://api.telegram.org/{bot_token}/sendMessage?chat_id={bot_ChatID}&text={botLogs}'
-
-    response = requests.get(bot_text)
-
-def main():
-    tgGetLogs("Process has started")
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36'
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument(f'user-agent={user_agent}')
-    
-    driver.get(site)
-
-    time.sleep(3)
-    try:
-        driver.find_element(By.CLASS_NAME, "checkbox").click()                                                  #checkbox
-        print('step 1')                                             
-        driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[2]/div[2]/a[1]').click()                 #Start button
-        print('step 2')
-        time.sleep(3)                 
-        driver.find_element(By.ID, "SiteID").click()                                                            #site selection
-        print('step 3')                                                            
-        Select(driver.find_element(By.ID, "SiteID")).select_by_index(10)                                        #select site number 10
-        print('step 4')
-        time.sleep(3)                                         
-        driver.find_element_by_xpath('//*[@id="pubpow-notif"]/label').click()                                 #agree tos
-        print('step 5')                                  
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "submitcommand"))).click()     #sumbit
-        print('Process done')  
-   
-        loop = True
-        sites = [1,2,3,4,5,7,8,9]
-        while loop:
-            for option in sites:
-                time.sleep(5)
-                timeSlots = driver.find_element(By.ID, "schedule-container").text
-                Select(driver.find_element(By.ID, "SiteID")).select_by_index(option)
-                sitename = ""
-                ####GET TIME####
-                today = datetime.now()
-                dateToday = today.strftime("%m/%d/%Y %I:%M %p")
-
-                if(option == 1):
-                    sitename = "Robinsons Las Pinas - Temporary Off-site Passport Service"
-                elif(option == 2):
-                    sitename = "Robinsons Magnolia - Temporary Off-site Passport Service"
-                elif(option == 3):
-                    sitename = "SM Aura - Temporary Off-site Passport Service"
-                elif(option == 4):
-                    sitename = "SM Mall of Asia - Temporary Off-site Passport Service"
-                elif(option == 5):
-                    sitename = "SM North Edsa -Temporary Off-site Passport Service"
-                elif(option == 7):
-                    sitename = "Newport Mall - Temporary Off-site Passport Service"
-                elif(option == 8):
-                    sitename = "San Pedro Laguna - Temporary Off-site Passport Service"
-                else:
-                    sitename = "SM Seaside Cebu - Temporary Off-site Passport Service"
-
-                if("Timeslots will be available soon." in timeSlots):
-                    tgGetLogs(f"NO APPOINTMENT AVAILABLE\n  \n{sitename}\n \n{dateToday}\n")
-                    print(f"\n*************************** NO APPOINTMENT AVAILABLE IN {sitename} ***************************\n")
-                    time.sleep(3)                   
-                else:
-                    print(f"\n********************** APPOINTMENT AVAILABLE IN {sitename} ***********************************\n")
-                    #sendMsg()
-                    sendTelegram(f' **New Appointment**\n \nSITE : {sitename}\n \n{dateToday}\n')  
-                    print("Message sent.")      
-    except (NoSuchElementException,TimeoutException) as e:
-        tgGetLogs(f"\nError occured during process.\n \nError Message: {e.args}\n")
-        driver.quit()
 
 @bot.message_handler(commands=['start'])
 def response(message):
@@ -123,12 +19,11 @@ def response(message):
 
 @bot.message_handler(commands=['sudostart'])
 def response(message):
-    bot.send_message(message.chat.id, 'Ok. It might take a while to initialize please be patient.')
-    main()
+    bot.send_message(message.chat.id, 'Service Initializing')
+    main.checkprocess()
 
 @bot.message_handler(commands=['sudostop'])
 def response(message):
-    bot.send_message(message.chat.id, 'Process terminated')
-    driver.quit()
+    main.closeWebdrv()
 
 bot.polling()
---
+++ dfa.py
@@ -0,0 +1,135 @@
+
+import time
+import os
+import requests
+from selenium import webdriver
+from datetime import datetime
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import InvalidSessionIdException
+from requests.exceptions import Timeout
+from urllib3.exceptions import MaxRetryError
+from urllib3.exceptions import ProtocolError
+
+#send msg to tg
+def sendTelegram(botMsg):
+    bot_token = "bot2023896048:AAE_MnkOljwcRXNXlC6ouEwrTpfYZVeRc1c"
+    bot_ChatID = "879252455"
+    bot_text = f'https://api.telegram.org/{bot_token}/sendMessage?chat_id={bot_ChatID}&text={botMsg}'
+
+    print("Sending message")
+
+    response = requests.get(bot_text)
+#logs to messenger
+def tgGetLogs(botLogs):
+    bot_token = "bot2054859695:AAGVSXp1MRtrAMP0L5g2AML-tBVvwRfxi4o"
+    bot_ChatID = "879252455"
+    bot_text = f'https://api.telegram.org/{bot_token}/sendMessage?chat_id={bot_ChatID}&text={botLogs}'
+
+    response = requests.get(bot_text)
+
+def webdrv():
+    global driver
+    site = "https://www.passport.gov.ph/appointment"
+    #path = "./chromedriver.exe"
+    chrome_options = webdriver.ChromeOptions()
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36'
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.headless = True
+    chrome_options.add_argument(f'user-agent={user_agent}')
+    driver = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    #driver = webdriver.Chrome(executable_path=path, options=chrome_options)
+    driver.get(site)
+    return driver
+
+
+def closeWebdrv():
+    try:
+        driver.quit()
+        
+        tgGetLogs("Service Terminated.\n\nIf you wish to restart the Service use /sudostart\n\n")
+    except Exception as e:
+        tgGetLogs(f"Service is not running.")
+
+def checkprocess():
+    webdrv()
+    try:      
+        #driver.find_element(By.CLASS_NAME, "checkbox").click()
+        driver.find_element_by_class_name('checkbox').click()                                              #checkbox
+        tgGetLogs('✅ Step 1.....Passed')
+        ######################################### 
+        driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[2]/div[2]/a[1]').click()                 #Start button
+        tgGetLogs('✅ Step 2.....Passed')
+        #########################################
+        driver.implicitly_wait(1)  
+        driver.find_element(By.ID, "SiteID").click()                                                           #site selection
+        tgGetLogs('✅ Step 3.....Passed')
+        #########################################  
+        Select(driver.find_element(By.ID, "SiteID")).select_by_index(10)                                        #select site number 10
+        tgGetLogs('✅ Step 4.....Passed')
+        #########################################
+        driver.implicitly_wait(5)
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#pubpow-notif-checkbox'))).click()
+        #driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div/form/div[3]/input').click()                           #agree tos
+        tgGetLogs('✅ Step 5.....Passed')
+        #########################################                                  
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "submitcommand"))).click()     #sumbit
+        tgGetLogs('✅ Initialization complete. checking started. ')     
+
+        loop = True
+        sites = [1,2,3,4,5,7,8,9]
+        while loop:
+            for option in sites:
+                sitename = "sitename"
+                ####GET TIME####
+                today = datetime.now()
+                dateToday = today.strftime("%m/%d/%Y %I:%M %p")
+                if(option == 1):
+                    sitename = "Robinsons Las Pinas - Temporary Off-site Passport Service"
+                elif(option == 2):
+                    sitename = "Robinsons Magnolia - Temporary Off-site Passport Service"
+                elif(option == 3):
+                    sitename = "SM Aura - Temporary Off-site Passport Service"
+                elif(option == 4):
+                    sitename = "SM Mall of Asia - Temporary Off-site Passport Service"
+                elif(option == 5):
+                    sitename = "SM North Edsa -Temporary Off-site Passport Service"
+                elif(option == 7):
+                    sitename = "Newport Mall - Temporary Off-site Passport Service"
+                elif(option == 8):
+                    sitename = "San Pedro Laguna - Temporary Off-site Passport Service"
+                else:
+                    sitename = "SM Seaside Cebu - Temporary Off-site Passport Service"
+                time.sleep(3)
+                #slot = driver.find_element(By.ID, "schedule-container").text
+                slot = driver.find_element_by_css_selector('#schedule-container').text
+                Select(driver.find_element(By.ID, "SiteID")).select_by_index(option)
+                if("Timeslots will be available soon." in slot):
+                    tgGetLogs(f"NO APPOINTMENT AVAILABLE\n  \n{sitename}\n \n{dateToday}\n")
+                    print(f"\n*************************** NO APPOINTMENT AVAILABLE IN {sitename} ***************************\n")              
+                else:
+                    print(f"\n********************** APPOINTMENT AVAILABLE IN {sitename} ***********************************\n")
+                    #sendMsg()
+                    sendTelegram(f' **New Appointment**\n \nSITE : {sitename}\n \n{dateToday}\n')  
+                    print("Message sent.")
+
+    except (ElementNotInteractableException, NoSuchElementException, TimeoutException, ElementClickInterceptedException, InvalidSessionIdException, Timeout) as e:
+        tgGetLogs(f'❌ Error occured:  {e.msg}\n\nuse /sudostart to restart the process.\n\n')
+    except (MaxRetryError, ProtocolError) as e:
+        print(f' Error occured: {e.args}\n\nuse /sudostart to restart the process.\n\n')
+    except ConnectionError as e:
+        print(f' Error occured: Connection aborted')
+    except AttributeError as e:
+        tgGetLogs(f'❌ Error occured:  {e.name}\n\nuse /sudostart to restart the process.\n\n')
+    finally:
+        closeWebdrv()
