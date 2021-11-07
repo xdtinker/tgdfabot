48332fd81d38fff587e4e6ace17bd6612cc08672
xdtinker <93765440+xdtinker@users.noreply.github.com>
2021-11-07 10:39:34 +0600

    Add files via upload

--- dfa.py
+++ dfa.py
@@ -38,19 +38,20 @@ def tgGetLogs(botLogs):
 def webdrv():
     global driver
     site = "https://www.passport.gov.ph/appointment"
-    #path = "./chromedriver.exe"
+    path = "./chromedriver.exe"
     chrome_options = webdriver.ChromeOptions()
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36'
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument('--headless')
     chrome_options.add_argument(f'user-agent={user_agent}')
-    driver = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-    #driver = webdriver.Chrome(executable_path=path, options=chrome_options)
+    #driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    driver = webdriver.Chrome(executable_path=path, options=chrome_options)
     driver.get(site)
+    return driver
 
-webdrv()
+
 def closeWebdrv():
     try:
         driver.quit()
@@ -59,11 +60,15 @@ def closeWebdrv():
     except Exception as e:
         tgGetLogs(f"Service is not running.")
 
+
+
 def checkprocess():
-    
+    tgGetLogs('XXXXXXXXXXXXX')
+    webdrv()
     try:      
         #driver.find_element(By.CLASS_NAME, "checkbox").click()
-        driver.find_element_by_class_name('checkbox').click()                                              #checkbox
+        driver.implicitly_wait(5)
+        driver.find_element_by_xpath('//*[@id="agree"]').click()                                         #checkbox
         tgGetLogs('✅ Step 1.....Passed')
         ######################################### 
         driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[2]/div[2]/a[1]').click()                 #Start button
