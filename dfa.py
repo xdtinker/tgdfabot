481eb4003ff308a93442fb397b6aa0c23e22e35f
xdtinker <93765440+xdtinker@users.noreply.github.com>
2021-11-07 11:58:33 +0800

    fix v2 typo

--- dfa.py
+++ dfa.py
@@ -40,8 +40,8 @@ def webdrv():
     global driver
     site = "https://www.passport.gov.ph/appointment"
     #path = "./chromedriver.exe"
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36'
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
