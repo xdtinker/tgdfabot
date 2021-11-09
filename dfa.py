eb832694dae1a1058ba7b19fff82d940f1543b42
xdtinker <93765440+xdtinker@users.noreply.github.com>
2021-11-09 13:38:37 +0800

    Update dfa.py

--- dfa.py
+++ dfa.py
@@ -44,14 +44,14 @@ def webdrv():
     chrome_options = webdriver.ChromeOptions()
 
     
-    #user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44'
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     #chrome_options.add_argument('--headless')
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument('--headless')
     #chrome_options.add_argument('--disable-gpu')
-    #chrome_options.add_argument(f'user-agent={user_agent}')
+    chrome_options.add_argument(f'user-agent={user_agent}')
     driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     #driver = webdriver.Chrome(executable_path=path, options=chrome_options)
     driver.get(site)
