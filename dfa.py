20825d875447b4f5d5a3193137b16510e2123dbd
xdtinker <93765440+xdtinker@users.noreply.github.com>
2021-11-09 13:41:18 +0800

    Update dfa.py

--- dfa.py
+++ dfa.py
@@ -48,7 +48,7 @@ def webdrv():
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     #chrome_options.add_argument('--headless')
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
+    #chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument('--headless')
     #chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument(f'user-agent={user_agent}')
