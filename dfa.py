039c1ea58480baef6a60750773abc3a8c20675a2
xdtinker <93765440+xdtinker@users.noreply.github.com>
2021-11-13 12:22:17 +0800

    Update dfa.py

--- dfa.py
+++ dfa.py
@@ -45,6 +45,7 @@ def webdrv():
     
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--window-size=1920x1080")
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
