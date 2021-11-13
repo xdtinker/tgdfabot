b84508b6ee8e177fe30230ed9bdce3e1fc579ec0
xdtinker <93765440+xdtinker@users.noreply.github.com>
2021-11-13 13:02:19 +0800

    Update dfa.py

--- dfa.py
+++ dfa.py
@@ -51,10 +51,10 @@ def checkprocess():
     site = "https://www.passport.gov.ph/appointment"
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument('--headless')
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
     
     driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     driver.get(site)
