e46a4e21a0f983d8edeaa7851181f623411f2fac
xdtinker <93765440+xdtinker@users.noreply.github.com>
2021-11-12 21:36:03 -0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,58 +1,29 @@
 import telebot
-from telebot import custom_filters
 import constant as keys
 import dfa as main
+
 
 bot = telebot.TeleBot(keys.API_KEY)
 
 @bot.message_handler(commands=['start'])
 def response(message):
-    bot.send_message(message.chat.id, "Greetings, {name}!".format(name=message.from_user.first_name))
+    bot.send_message(message.chat.id, 'Greetings, Human.')
 
-@bot.message_handler(text=['hi' ,'hello'])
+@bot.message_handler(commands=['hi'])
 def response(message):
-    bot.send_message(message.chat.id, "Hi, {name}!")
+    bot.send_message(message.chat.id, 'Hi Dev')
  
 @bot.message_handler(commands=['help'])
 def response(message):
-    bot.send_message(message.chat.id, "I Can't help you at the moment.")
-
-@bot.message_handler(chat_id=[keys.DEV_ID], commands=['sudostart'])
-def response(message):
-    bot.send_message(message.chat.id, 'Ok')
-    main.checkprocess()
+    bot.send_message(message.chat.id, "There's no help")
 
 @bot.message_handler(commands=['sudostart'])
 def response(message):
-    bot.send_message(message.chat.id, 'Access Denied.')
-
-@bot.message_handler(chat_id=[keys.DEV_ID], commands=['sudostop'])
-def response(message):
-    bot.send_message(message.chat.id, 'Ok')
-    main.closeWebdrv()
+    bot.send_message(message.chat.id, 'Service Initializing')
+    main.checkprocess()
 
 @bot.message_handler(commands=['sudostop'])
 def response(message):
-    bot.send_message(message.chat.id, 'Access Denied.')
-
-@bot.message_handler(commands=['status'])
-def response(message):
-    bot.send_message(message.chat.id, "I'm Alive")
-
-@bot.message_handler(commands=['commands'])
-def response(message):
-    bot.send_message(message.chat.id,
-    """Available commands:
-
-/start > Greetings
-/help > help?
-/status > Check Bot status
-/sudostart > Start Session
-/sudostop > Close Session   
-    """)
-
-bot.add_custom_filter(custom_filters.ChatFilter())
-bot.add_custom_filter(custom_filters.TextMatchFilter())
-bot.add_custom_filter(custom_filters.TextStartsFilter())
+    main.closeWebdrv()
 
 bot.polling()
--- dfa.py
+++ dfa.py
@@ -1,7 +1,6 @@
 import time
 import os
 import requests
-import constant as keys
 from selenium import webdriver
 from datetime import datetime
 from selenium.webdriver.common.by import By
@@ -15,17 +14,15 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.common.exceptions import ElementNotInteractableException
 from selenium.common.exceptions import InvalidSessionIdException
-from selenium.common.exceptions import WebDriverException
 from requests.exceptions import Timeout
-from requests.exceptions import ReadTimeout
 from urllib3.exceptions import MaxRetryError
 from urllib3.exceptions import ProtocolError
-
 
 #send msg to tg
 def sendTelegram(botMsg):
     bot_token = "bot2023896048:AAE_MnkOljwcRXNXlC6ouEwrTpfYZVeRc1c"
-    bot_text = f'https://api.telegram.org/{bot_token}/sendMessage?chat_id={keys.BOT_CHANNEL_APT}&text={botMsg}'
+    bot_ChatID = "879252455"
+    bot_text = f'https://api.telegram.org/{bot_token}/sendMessage?chat_id={bot_ChatID}&text={botMsg}'
 
     print("Sending message")
 
@@ -33,27 +30,26 @@ def sendTelegram(botMsg):
 #logs to messenger
 def tgGetLogs(botLogs):
     bot_token = "bot2054859695:AAGVSXp1MRtrAMP0L5g2AML-tBVvwRfxi4o"
-    bot_text = f'https://api.telegram.org/{bot_token}/sendMessage?chat_id={keys.BOT_CHANNEL_LOG}&text={botLogs}'
+    bot_ChatID = "879252455"
+    bot_text = f'https://api.telegram.org/{bot_token}/sendMessage?chat_id={bot_ChatID}&text={botLogs}'
 
     response = requests.get(bot_text)
 
 def webdrv():
     global driver
     site = "https://www.passport.gov.ph/appointment"
-    #path = "./chromedriver.exe"
+    path = "./chromedriver.exe"
     chrome_options = webdriver.ChromeOptions()
-    
-    #user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36'
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    #chrome_options.add_argument("--window-size=1920x1080")
-    #chrome_options.add_argument('--headless')
-    #chrome_options.add_argument('--no-sandbox')
-    #chrome_options.add_argument('--disable-dev-shm-usage')
-    #chrome_options.add_argument(f'user-agent={user_agent}')
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument(f'user-agent={user_agent}')
     driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     #driver = webdriver.Chrome(executable_path=path, options=chrome_options)
     driver.get(site)
-    return chrome_options
+    return driver
 
 
 def closeWebdrv():
@@ -67,47 +63,40 @@ def closeWebdrv():
 
 
 def checkprocess():
-    tgGetLogs('Checking in progress..')
+    tgGetLogs('XXXXXXXXXXXXX')
     webdrv()
-    try:
-        #driver.implicitly_wait(10)
-        #driver.find_element_by_xpath("//input[@type='checkbox']").click()
+    try:      
         #driver.find_element(By.CLASS_NAME, "checkbox").click()
-        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='checkbox']"))).click()
-        #driver.find_element_by_xpath('//*[@id="agree"]').click()                                         #checkbox
+        driver.implicitly_wait(5)
+        driver.find_element_by_xpath('//*[@id="agree"]').click()                                         #checkbox
         tgGetLogs('✅ Step 1.....Passed')
         ######################################### 
         driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[2]/div[2]/a[1]').click()                 #Start button
         tgGetLogs('✅ Step 2.....Passed')
         #########################################
-        time.sleep(2)
+        driver.implicitly_wait(1)  
         driver.find_element(By.ID, "SiteID").click()                                                           #site selection
         tgGetLogs('✅ Step 3.....Passed')
         #########################################  
         Select(driver.find_element(By.ID, "SiteID")).select_by_index(10)                                        #select site number 10
         tgGetLogs('✅ Step 4.....Passed')
         #########################################
-        #WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#pubpow-notif-checkbox'))).click()
-        #driver.find_element_by_xpath("//input[@type='checkbox']").click()
-        time.sleep(2)
-        driver.find_element_by_id('pubpow-notif-checkbox').click()           #agree tos
+        driver.implicitly_wait(5)
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#pubpow-notif-checkbox'))).click()
+        #driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div/form/div[3]/input').click()                           #agree tos
         tgGetLogs('✅ Step 5.....Passed')
         #########################################                                  
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "submitcommand"))).click()     #sumbit
-        tgGetLogs('✅ Initialization complete. checking started. ')
+        tgGetLogs('✅ Initialization complete. checking started. ')     
 
         loop = True
         sites = [1,2,3,4,5,7,8,9]
         while loop:
             for option in sites:
                 sitename = "sitename"
-                time.sleep(3)
-                slot = driver.find_element_by_xpath("//*[contains(text(), 'Timeslots will be available soon.')]").text
-                #slot = driver.find_element_by_css_selector('#schedule-container').text
-                Select(driver.find_element(By.ID, "SiteID")).select_by_index(option)
                 ####GET TIME####
                 today = datetime.now()
-                dateToday = today.strftime("%m/%d/%Y")
+                dateToday = today.strftime("%m/%d/%Y %I:%M %p")
                 if(option == 1):
                     sitename = "Robinsons Las Pinas - Temporary Off-site Passport Service"
                 elif(option == 2):
@@ -124,7 +113,10 @@ def checkprocess():
                     sitename = "San Pedro Laguna - Temporary Off-site Passport Service"
                 else:
                     sitename = "SM Seaside Cebu - Temporary Off-site Passport Service"
-                
+                time.sleep(3)
+                #slot = driver.find_element(By.ID, "schedule-container").text
+                slot = driver.find_element_by_css_selector('#schedule-container').text
+                Select(driver.find_element(By.ID, "SiteID")).select_by_index(option)
                 if("Timeslots will be available soon." in slot):
                     tgGetLogs(f"NO APPOINTMENT AVAILABLE\n  \n{sitename}\n \n{dateToday}\n")
                     print(f"\n*************************** NO APPOINTMENT AVAILABLE IN {sitename} ***************************\n")              
@@ -134,19 +126,13 @@ def checkprocess():
                     sendTelegram(f' **New Appointment**\n \nSITE : {sitename}\n \n{dateToday}\n')  
                     print("Message sent.")
 
-    except (ElementNotInteractableException, NoSuchElementException, TimeoutException, ElementClickInterceptedException, InvalidSessionIdException, Timeout, ReadTimeout, WebDriverException) as e:
-        tgGetLogs(f'❌ Error occured:  {e.__dict__}\n\nuse /sudostart to restart the process.\n\n')
-        print(f'❌ Error occured:  {e.__dict__}\n\nuse /sudostart to restart the process.\n\n')
+    except (ElementNotInteractableException, NoSuchElementException, TimeoutException, ElementClickInterceptedException, InvalidSessionIdException, Timeout) as e:
+        tgGetLogs(f'❌ Error occured:  {e.msg}\n\nuse /sudostart to restart the process.\n\n')
     except (MaxRetryError, ProtocolError) as e:
-        print(f' Error occured: {e.__dict__}\n\nuse /sudostart to restart the process.\n\n')
+        print(f' Error occured: {e.args}\n\nuse /sudostart to restart the process.\n\n')
     except ConnectionError as e:
         print(f' Error occured: Connection aborted')
     except AttributeError as e:
         tgGetLogs(f'❌ Error occured:  {e.name}\n\nuse /sudostart to restart the process.\n\n')
-        print(f'❌ Error occured:  {e.name}\n\nuse /sudostart to restart the process.\n\n')
     finally:
-        tgGetLogs(f'Closing session...')
-        print(f'Closing session...')
-        driver.quit()
-        tgGetLogs(f'Session closed.')
-        print(f'Session closed.')
+        closeWebdrv()
