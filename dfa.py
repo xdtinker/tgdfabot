285162eaa8a625d1de4796c9d78122c30a4d9b9c
xdtinker <93765440+xdtinker@users.noreply.github.com>
2021-11-15 17:05:21 +0800

    Update dfa.py

--- dfa.py
+++ dfa.py
@@ -38,17 +38,16 @@ def tgGetLogs(botLogs):
 def webdrv():
     global driver
     site = "https://www.passport.gov.ph/appointment"
-    path = "./chromedriver.exe"
-    #chrome_options = webdriver.ChromeOptions()
-    chrome_options = Options()
+    
+    chrome_options = webdriver.ChromeOptions()
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument('--headless')
     chrome_options.add_argument(f'user-agent={user_agent}')
-    driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-    #driver = webdriver.Chrome(executable_path=path, options=chrome_options)
+    
+    driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(site)
     return driver
 
