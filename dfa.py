05304fcf23a06a046c0050a3e38d8ece8b8edf3e
xdtinker <93765440+xdtinker@users.noreply.github.com>
2021-11-15 17:59:33 +0800

    Update dfa.py

--- dfa.py
+++ dfa.py
@@ -52,7 +52,7 @@ def webdrv():
     
     chrome_options.add_experimental_option("prefs",prefs)
     
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
